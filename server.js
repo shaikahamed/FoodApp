abda2f923f26cdad86b42c9d976c915cfe077471
Shaik Zunaid Ahamed <zunaidahamed786@gmail.com>
2022-08-19 14:47:26 +0530

    created schema, model and entry on mongoatlas

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express();
 
+app.use(express.json());
 
 const userModel = require('./userModel');
 
@@ -11,3 +12,13 @@ app.listen(3000, function(){
     console.log("Server started at port 3000");
 })
 
+//post request from the user
+app.post('/signup', async function(req, res){
+    let data = req.body;
+    console.log(data);
+    let newUser = userModel.create(data);
+    res.json({
+        message: "Data received",
+        data: data
+    })
+})
--- userModel.js
+++ userModel.js
@@ -9,3 +9,41 @@ mongoose.connect(dbAuth).then(function(){
 }).catch(function(err){
     console.log("Error in dbConnection :", err);
 })
+
+//first create a schema for the database 
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    confirmPassword:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNo:{
+        type: String,
+        maxLength:10, 
+        minLength:10
+    },
+    pic:{
+        type: String,
+        default:"logo2.png"
+    }
+});
+
+//Schemas should be converted into models to work with
+
+const userModel = mongoose.model("FoodUserModel", userSchema);
+module.exports = userModel;
