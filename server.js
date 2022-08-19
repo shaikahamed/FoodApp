1aff905a661b1f069fc6e9c30e1a4cb8b00d5b48
Shaik Zunaid Ahamed <zunaidahamed786@gmail.com>
2022-08-19 14:50:25 +0530

    try catch for /signup post

--- server.js
+++ server.js
@@ -8,17 +8,22 @@ app.use(express.json());
 const userModel = require('./userModel');
 
 //listen
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started at port 3000");
 })
 
 //post request from the user
-app.post('/signup', async function(req, res){
-    let data = req.body;
-    console.log(data);
-    let newUser = userModel.create(data);
-    res.json({
-        message: "Data received",
-        data: data
-    })
+app.post('/signup', async function (req, res) {
+    try {
+        let data = req.body;
+        console.log(data);
+        let newUser = userModel.create(data);
+        res.json({
+            message: "Data received",
+            data: data
+        })
+    }catch(err){
+        res.send(err.message);
+    }
+    
 })
